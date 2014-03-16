4cf47954b965d9bf5db8c851bfac729b064e109c
Weerasak Chongnguluam <singpor@gmail.com>
2014-03-16 14:54:50 +0700

    Simple pass test 7

--- src/main/java/kata/sort/MySort.java
+++ src/main/java/kata/sort/MySort.java
@@ -8,12 +8,19 @@ public class MySort {
 
         int listLength = list.length;
         if (listLength == 2) {
-                int leftNumber = (Integer) list[0];
-                int rightNumber = (Integer) list[1];
-                if (leftNumber > rightNumber) {
-                    list[0] = rightNumber;
-                    list[1] = leftNumber;
-                }
+            int leftNumber = (Integer) list[0];
+            int rightNumber = (Integer) list[1];
+            if (leftNumber > rightNumber) {
+                list[0] = rightNumber;
+                list[1] = leftNumber;
+            }
+        } else if (listLength == 3) {
+            int leftNumber = (Integer) list[1];
+            int rightNumber = (Integer) list[2];
+            if (leftNumber > rightNumber) {
+                list[1] = rightNumber;
+                list[2] = leftNumber;
+            }
         }
 
         return list;
