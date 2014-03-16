d7ec42ed7df84450d1c95a73e2d76e57b591769c
Somkiat <somkiat.p@gmail.com>
2014-03-16 23:38:17 +0700

    Add test case for 3 elements

--- src/test/java/kata/sort/MySortTest.java
+++ src/test/java/kata/sort/MySortTest.java
@@ -44,16 +44,16 @@ public class MySortTest {
 		assertArrayEquals(list(1, 2, 3), mySort.sort(list(1, 2, 3)));
 	}
 
+//	@Test
+//	public void listOfThreeShouldReturnOrderedList2() throws Exception {
+//		assertArrayEquals(list(1, 2, 3), mySort.sort(list(1, 3, 2)));
+//	}
+
 	@Test
-	public void listOfThreeShouldReturnOrderedList2() throws Exception {
-		assertArrayEquals(list(1, 2, 3), mySort.sort(list(1, 3, 2)));
+	public void listOfThreeShouldReturnOrderedList_() throws Exception {
+		assertArrayEquals(list(1, 2, 3), mySort.sort(list(2, 1, 3)));
 	}
 
-//	@Test
-//	public void listOfThreeShouldReturnOrderedList_() throws Exception {
-//		assertArrayEquals(list(1, 2, 3), mySort.sort(list(2, 1, 3)));
-//	}
-//
 //	@Test
 //	public void listOfThreeShouldReturnOrderedList4() throws Exception {
 //		assertArrayEquals(list(1, 2, 3), mySort.sort(list(3, 2, 1)));
