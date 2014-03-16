bad763430a4a44123bf45206d3cd999ddfad95b0
karan <bomb0069@gmail.com>
2014-03-17 01:28:01 +0700

    refactor code by use single if and create listToArray

--- src/main/java/kata/sort/MySort.java
+++ src/main/java/kata/sort/MySort.java
@@ -4,34 +4,36 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class MySort {
+
     public Integer[] sort(Integer[] numbers) {
-        if (numbers != null) {
-        	if (numbers.length > 1) {
-	        	Integer firstValue = numbers[0];
+        if (numbers != null && numbers.length > 1) {
+        	Integer firstValue = numbers[0];
 
-	        	List<Integer> firstHalfList = new ArrayList<Integer>();	
-	        	List<Integer> secondHalfList = new ArrayList<Integer>();
+        	List<Integer> firstHalfList = new ArrayList<Integer>();	
+        	List<Integer> secondHalfList = new ArrayList<Integer>();
 
-	        	for (Integer number : numbers) {
-	        		if (number < firstValue) {
-	        			firstHalfList.add(number);
-	        		} else if (number > firstValue) {
-	        			secondHalfList.add(number);
-	        		}
-	        	}
+        	for (Integer number : numbers) {
+        		if (number < firstValue) {
+        			firstHalfList.add(number);
+        		} else if (number > firstValue) {
+        			secondHalfList.add(number);
+        		}
+        	}
 
-	        	firstHalfList.add(firstValue);
+        	firstHalfList.add(firstValue);
 
-	        	Integer[] firstHalfArray = sort(firstHalfList.toArray(new Integer[firstHalfList.size()]));
-	        	Integer[] secondHalfArray = sort(secondHalfList.toArray(new Integer[secondHalfList.size()]));
+        	Integer[] firstHalfArray = sort(listToArray(firstHalfList));
+        	Integer[] secondHalfArray = sort(listToArray(secondHalfList));
 
-	        	numbers = new Integer[firstHalfArray.length + secondHalfArray.length];
-
-	        	System.arraycopy(firstHalfArray, 0, numbers, 0, firstHalfArray.length) ;
-	        	System.arraycopy(secondHalfArray, 0, numbers, firstHalfArray.length, secondHalfArray.length) ;
-	        }
+        	System.arraycopy(firstHalfArray, 0, numbers, 0, firstHalfArray.length) ;
+        	System.arraycopy(secondHalfArray, 0, numbers, firstHalfArray.length, secondHalfArray.length) ;
         }
         return numbers;
+    }
+
+    private Integer[] listToArray (List<Integer> integerList) {
+		return integerList.toArray(new Integer[integerList.size()]);	
+
     }
 
 }
