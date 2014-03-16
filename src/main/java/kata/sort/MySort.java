6de5240791dbdab471c07f584cc62d7bc2043e3f
karan <bomb0069@gmail.com>
2014-03-16 23:14:32 +0700

    solved listOfTwoThatNotOrderShouldReturnOrderedList

--- src/main/java/kata/sort/MySort.java
+++ src/main/java/kata/sort/MySort.java
@@ -2,6 +2,33 @@ package kata.sort;
 
 public class MySort {
     public Integer[] sort(Integer[] numbers) {
-        return numbers;
+    	if (numbers == null)
+    		return null;
+
+    	Integer[] sortedArray = new Integer[numbers.length];
+
+    	Integer position = 0;
+   		Integer minValue = 0;
+
+    	for (Integer numberForMin : numbers) {
+    		Integer newMinValue = getNewMinValue(numbers, minValue);
+			sortedArray[position++] = newMinValue;
+			minValue = newMinValue;
+    	}
+        return sortedArray;
     }
+
+    private Integer getNewMinValue(Integer[] numbers, Integer oldMinValue) {
+    	Integer newMinValue = null;
+		for (Integer innerNumber : numbers) {
+			if (innerNumber > oldMinValue) {
+				if (newMinValue == null)
+					newMinValue = innerNumber;
+				if (innerNumber < newMinValue)
+					newMinValue = innerNumber;
+			}
+		}
+		return newMinValue;
+    }
+
 }
