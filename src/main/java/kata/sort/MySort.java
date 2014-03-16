8b66c4383b1ffd2a6bf315b8e81b5db7ced808ad
karan <bomb0069@gmail.com>
2014-03-17 00:34:15 +0700

    solved listOfThreeShouldReturnOrderedList_ by seperate 2 list from first element and merge againt at the end;

--- src/main/java/kata/sort/MySort.java
+++ src/main/java/kata/sort/MySort.java
@@ -1,11 +1,28 @@
 package kata.sort;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class MySort {
     public Integer[] sort(Integer[] numbers) {
         if (numbers != null) {
-            if ((numbers.length == 2) && (numbers[0] > numbers[1])) {
-                return new Integer[] { numbers[1], numbers[0] };
-            }
+        	Integer firstValue = numbers[0];
+
+        	List firstHalfList = new ArrayList<Integer>();	
+        	List secondHalfList = new ArrayList<Integer>();
+
+        	for (Integer number : numbers) {
+        		if (number < firstValue) {
+        			firstHalfList.add(number);
+        		} else if (number > firstValue) {
+        			secondHalfList.add(number);
+        		}
+        	}
+        	firstHalfList.add(firstValue);
+        	firstHalfList.addAll(secondHalfList);
+
+        	numbers = (Integer[])firstHalfList.toArray(new Integer[firstHalfList.size()]);
+
         }
         return numbers;
     }
