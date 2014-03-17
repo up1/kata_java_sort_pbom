c3bb7f0f7fbdfdaa6933f04868fa66f799307664
karan <bomb0069@gmail.com>
2014-03-18 02:26:28 +0700

    refactor by use organize import fro eclipse

--- src/main/java/kata/sort/MySort.java
+++ src/main/java/kata/sort/MySort.java
@@ -1,9 +1,8 @@
 package kata.sort;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MySort {
 
