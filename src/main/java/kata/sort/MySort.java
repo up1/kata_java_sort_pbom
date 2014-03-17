d0534cb8366f792a301d2ef43220fe3dfa19a58b
Somkiat <somkiat.p@gmail.com>
2014-03-17 09:18:23 +0700

    Refactoring by create new method for operate with List operation instead of Array, it's look beeter

--- src/main/java/kata/sort/MySort.java
+++ src/main/java/kata/sort/MySort.java
@@ -1,37 +1,41 @@
 package kata.sort;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 
 public class MySort {
-    public Integer[] sort(Integer[] numbers) {
-        if (numbers != null) {
-        	if (numbers.length > 1) {
-	        	Integer firstValue = numbers[0];
+	public Integer[] sort(Integer[] numbers) {
+		if (numbers != null && numbers.length > 1) {
+			return sort(Arrays.asList(numbers)).toArray(new Integer[numbers.length]);
+		}
+		return numbers;
+	}
 
-	        	List<Integer> firstHalfList = new ArrayList<Integer>();	
-	        	List<Integer> secondHalfList = new ArrayList<Integer>();
+	private List<Integer> sort(List<Integer> numbers) {
+		if( numbers.size() == 0 ) return numbers;
+		
+		Integer firstValue = numbers.get(0);
+		List<Integer> firstHalfList = new ArrayList<Integer>();
+		List<Integer> secondHalfList = new ArrayList<Integer>();
 
-	        	for (Integer number : numbers) {
-	        		if (number < firstValue) {
-	        			firstHalfList.add(number);
-	        		} else if (number > firstValue) {
-	        			secondHalfList.add(number);
-	        		}
-	        	}
+		for (Integer number : numbers) {
+			if (number < firstValue) {
+				firstHalfList.add(number);
+			} else if (number > firstValue) {
+				secondHalfList.add(number);
+			}
+		}
+		return makeResultList(sort(firstHalfList), firstValue, sort(secondHalfList));
+	}
 
-	        	firstHalfList.add(firstValue);
-
-	        Integer[] firstHalfArray = sort(firstHalfList.toArray(new Integer[firstHalfList.size()]));
-	        	Integer[] secondHalfArray = sort(secondHalfList.toArray(new Integer[secondHalfList.size()]));
-
-	        	numbers = new Integer[firstHalfArray.length + secondHalfArray.length];
-
-	        	System.arraycopy(firstHalfArray, 0, numbers, 0, firstHalfArray.length) ;
-	        	System.arraycopy(secondHalfArray, 0, numbers, firstHalfArray.length, secondHalfArray.length) ;
-	        }
-        }
-        return numbers;
-    }
+	private List<Integer> makeResultList(List<Integer> leftList, Integer midNumber, List<Integer> rightList) {
+		ArrayList<Integer> resultList = new ArrayList<Integer>();
+		resultList.addAll(leftList);
+		resultList.add(midNumber);
+		resultList.addAll(rightList);
+		return resultList;
+	}
 
 }
