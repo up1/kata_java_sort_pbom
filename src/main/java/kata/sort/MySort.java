1dda268eafbe603468f23cad993a04393905ada6
karan <bomb0069@gmail.com>
2014-03-18 01:01:37 +0700

    refactor: extract getLower and HighList into private method

--- src/main/java/kata/sort/MySort.java
+++ src/main/java/kata/sort/MySort.java
@@ -18,17 +18,10 @@ public class MySort {
 		if( numbers.size() == 0 ) return numbers;
 		
 		Integer firstValue = numbers.get(0);
-		List<Integer> firstHalfList = new ArrayList<Integer>();
-		List<Integer> secondHalfList = new ArrayList<Integer>();
+		List<Integer> lowerFirstValueList = getLowerValueList(numbers, firstValue);
+		List<Integer> higherFirstValueList = getHigherValueList(numbers, firstValue);
 
-		for (Integer number : numbers) {
-			if (number < firstValue) {
-				firstHalfList.add(number);
-			} else if (number > firstValue) {
-				secondHalfList.add(number);
-			}
-		}
-		return makeResultList(sort(firstHalfList), firstValue, sort(secondHalfList));
+		return makeResultList(sort(lowerFirstValueList), firstValue, sort(higherFirstValueList));
 	}
 
 	private List<Integer> makeResultList(List<Integer> leftList, Integer midNumber, List<Integer> rightList) {
@@ -37,5 +30,25 @@ public class MySort {
 		resultList.add(midNumber);
 		resultList.addAll(rightList);
 		return resultList;
+	}
+
+	private List<Integer> getLowerValueList (List<Integer> numbers, Integer compareValue) {
+		List<Integer> lowerValueList = new ArrayList<Integer>();
+		for (Integer number : numbers) {
+			if (number < compareValue) {
+				lowerValueList.add(number);
+			}
+		}
+		return lowerValueList;
+	}
+
+	private List<Integer> getHigherValueList (List<Integer> numbers, Integer compareValue) {
+		List<Integer> higherValueList = new ArrayList<Integer>();
+		for (Integer number : numbers) {
+			if (number > compareValue) {
+				higherValueList.add(number);
+			}
+		}
+		return higherValueList;
 	}
 }
